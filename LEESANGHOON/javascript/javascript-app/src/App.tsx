2e312643482ee5815827a84fac640ab37da318d0
SanghoonLee <lshoheix@gmail.com>
2025-12-31 16:12:44 +0900

    feat: [이상훈] 배열 자르기(slice)[PMIF-x]

--- LEESANGHOON/javascript/javascript-app/src/App.tsx
+++ LEESANGHOON/javascript/javascript-app/src/App.tsx
@@ -11,6 +11,9 @@ import { ControlFlowForSumExample } from './control_flow/ControlFlowForSumExampl
 import { SecondProblem } from './problem/SecondProblem'
 import { MapExample } from './map/MapExample'
 import { MapReduceExample } from './map/MapReduceExample'
+import { MapFilterExample } from './map/MapFilterExample'
+import { ArraySliceExample } from './array/ArraySliceExample'
+
 
 
 function App() {
@@ -29,6 +32,8 @@ function App() {
       <SecondProblem/>
       <MapExample/>
       <MapReduceExample/>
+      <MapFilterExample/>
+      <ArraySliceExample/>
       </div>
     </>
   )
--- LEESANGHOON/javascript/javascript-app/src/map/MapFilterExample.tsx
+++ LEESANGHOON/javascript/javascript-app/src/map/MapFilterExample.tsx
@@ -1,18 +1,18 @@
 // 3 6 9 12 15 더한 결과 출력
 
-export const MapReduceExample = () =>{
+export const MapFilterExample = () =>{
 
     // number[] : typescript 특성(type을 명시하는 작업, 숫자 배열임을 의미)
     // number_array.map(): 배열 안의 요소들을 1개씩 꺼내옴 2,4,6,8,10 -> 각 요소의 제곱을 함
     let number_array: number[] = [2,4,6,8,10]
 
-    let result_array: number =
-        number_array.reduce((accumulator,element) => accumulator + element, 0)
+    let result_array: number[] =
+        number_array.filter(element => element % 4 === 0)
 
 
 return(
     <div>
-    <h3>javascript Reduce Function</h3>
+    <h3>javascript Filter Function</h3>
         <pre>{
     `
 number_array = ${number_array}
